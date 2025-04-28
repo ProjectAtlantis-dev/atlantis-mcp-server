79ceb5834c1633b0e1f0f7c700f43704bed603a8
Rein <rein@catatonic.ai>
2025-04-28 14:41:31 +0300

    save

--- python-server/dynamic_functions/game_chat.py
+++ python-server/dynamic_functions/game_chat.py
@@ -1,12 +1,12 @@
 import json
 from typing import List, Dict, Any
 
-
-async def game_chat(sceneEvents: List[Dict[str, Any]], event: Dict[str, Any]):
+#async def game_chat(sceneEvents: List[Dict[str, Any]], event: Dict[str, Any]):
+async def game_chat():
     '''
         this is a test
     '''
-    await client_log(f"hello")
+    client_log(f"hello")
 
 
     return "How was your day?"

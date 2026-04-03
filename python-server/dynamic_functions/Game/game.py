95bbdfc05b21c0551da77b27515a62060c3d3585
Juggernaut <rein@catatonic.ai>
2026-04-04 02:18:55 +0300

    save

--- python-server/dynamic_functions/Game/game.py
+++ python-server/dynamic_functions/Game/game.py
@@ -1,17 +1,46 @@
 import atlantis
 import logging
+import os
+
 
 logger = logging.getLogger("mcp_server")
 
 
 @game
 async def game():
-    """
-    This is a placeholder function for 'game'
-    """
-    logger.info(f"Executing placeholder function: game...")
+    """Initializes a new chat session"""
 
-    await atlantis.client_log("game running")
+    await atlantis.client_command("/silent on")
 
-    # Replace this return statement with your function's result
-    return f"Placeholder function 'game' executed successfully."
+    # get user id ('brickhouse')
+    user_id = atlantis.get_caller()
+    logger.info(f"Game started for user: {user_id}")
+
+    owner_id = atlantis.get_owner()
+    #await atlantis.client_log(f"Owner ID: {owner_id}")  # TEMP
+
+    #kittyPath = f"{owner_id}**Bot.Kitty.OpenRouterGLM**chat"
+    #kittyPath = f"{owner_id}**Bot.Kitty.OpenRouterMinimax**chat"
+    #await atlantis.client_command("/chat set " + kittyPath)
+
+    # set background
+    await atlantis.client_command("/silent off")
+    image_path = os.path.join(os.path.dirname(__file__), "builder.jpg")
+
+
+
+    await atlantis.set_background(image_path)
+
+
+
+
+    # send kitty face image
+
+    #kitty_path = os.path.join(os.path.dirname(__file__), "kitty_face_compressed.jpg")
+    #await atlantis.client_image(kitty_path)
+
+
+
+    #await atlantis.client_log(f"Kitty is at the front desk!")
+
+
